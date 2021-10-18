cb2cf40daa97b016cf2c796eec0cd2f13f22487e
sscoltock@simplemachines.com.au <sscoltock@simplemachines.com.au>
2021-10-18 00:50:27 +0000

    can run dim_customers

---
+++ models/example/stg_customers.sql
@@ -0,0 +1,6 @@
+    select
+        id as customer_id,
+        first_name,
+        last_name
+
+    from `rare-palace-329321.jaffle_shop.customers`
---
+++ models/example/stg_orders.sql
@@ -0,0 +1,9 @@
+
+
+    select
+        id as order_id,
+        user_id as customer_id,
+        order_date,
+        status
+
+    from `rare-palace-329321.jaffle_shop.orders`
