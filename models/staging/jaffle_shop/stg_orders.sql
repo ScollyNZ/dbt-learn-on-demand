e301dbe96c40df0625ab0c67acd527ed768a6d15
sscoltock@simplemachines.com.au <sscoltock@simplemachines.com.au>
2021-10-18 21:50:59 +0000

    basic tests in place

--- models/staging/jaffle_shop/stg_orders.sql
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -1,9 +1,15 @@
 
 
-    select
+    /*select
         id as order_id,
         user_id as customer_id,
         order_date,
+        status
+
+    from `rare-palace-329321.jaffle_shop.orders`*/
+
+    select
+        distinct 
         status
 
     from `rare-palace-329321.jaffle_shop.orders`
